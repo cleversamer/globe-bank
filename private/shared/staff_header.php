f94e1e5b621a1f45ef17844d8b3e63f208f2bce0
cleversamerr <thedev.samer@gmail.com>
2024-01-11 12:04:09 +0200

    separated content into single files and included them in the staff main web page

---
+++ private/shared/staff_header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" media="all" href="/public/stylesheets/staff.css" />
+    <title>GBI</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>GBI Staff Area</h1>
+    </header>
+
+    <navigation>
+        <ul>
+            <li>
+                <a href="index.php">Menu</a>
+            </li>
+        </ul>
+    </navigation>
--- public/staff/index.php
+++ public/staff/index.php
@@ -1,34 +1,7 @@
-<!doctype html>
+<?php require_once("../../private/initialize.php"); ?>
 
-<html lang="en">
+<?php include("../../private/shared/staff_header.php"); ?>
 
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" media="all" href="/public/stylesheets/staff.css" />
-    <title>GBI</title>
-</head>
+<div id="content"></div>
 
-<body>
-
-    <header>
-        <h1>GBI Staff Area</h1>
-    </header>
-
-    <navigation>
-        <ul>
-            <li>
-                <a href="index.php">Menu</a>
-            </li>
-        </ul>
-    </navigation>
-
-    <div id="content"></div>
-
-    <footer>
-        &copy;
-        <?php echo date("Y"); ?> Globe Bank
-    </footer>
-
-</body>
-
-</html>
+<?php include("../../private/shared/staff_footer.php"); ?>
